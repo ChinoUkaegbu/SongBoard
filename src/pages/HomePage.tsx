6e75b051144188e1340e182ad581d3e36593f0dd
ChinoU <chinoukaegbu@gmail.com>
2024-07-31 15:25:28 +0400

    add pinterest redirect for authorization

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,17 @@ import { Button, Center, Flex, Heading } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import { Link } from "react-router-dom";
 
-function HomePage() {
+const HomePage = () => {
+  const redirectToPinterest = () => {
+    const clientId = "1501989";
+    const redirectUri = "http://localhost:3000/auth/pinterest/callback"; // backend endpoint
+    const responseType = "code";
+    const scope = "boards:read,pins:read"; // scope
+    const authUrl = `https://www.pinterest.com/oauth/?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+
+    window.location.href = authUrl;
+  };
+
   return (
     <>
       <Heading
@@ -17,11 +27,11 @@ function HomePage() {
         vibe
       </Heading>
       <Center>
-        <Link to="/login">
-          <Button colorScheme="blackAlpha">Get Started</Button>
-        </Link>
+        <Button colorScheme="blackAlpha" onClick={redirectToPinterest}>
+          Get Started
+        </Button>
       </Center>
     </>
   );
-}
+};
 export default HomePage;
