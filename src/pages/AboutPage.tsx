b8d772e39f8aefd18db8e8a0b315e3d715969976
ChinoU <chinoukaegbu@gmail.com>
2024-08-02 15:46:32 +0400

    adding basic about page content

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,17 @@
+import { Flex, Heading, Image, Spacer } from "@chakra-ui/react";
+import musicListener from "../assets/music_listener.svg";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <Flex padding={5}>
+      <Heading>
+        a picture speaks a thousand words <br />
+        so should your playlists
+      </Heading>
+      <Spacer />
+      <Image src={musicListener} />
+    </Flex>
+  );
 };
 
 export default AboutPage;
