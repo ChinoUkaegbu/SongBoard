b7e07741123b42683634cb4cf207c6452ff6dca3
ChinoU <chinoukaegbu@gmail.com>
2024-08-03 10:23:25 +0400

    style about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,14 +1,18 @@
-import { Flex, Heading, Image, Spacer } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Image, Spacer } from "@chakra-ui/react";
 import musicListener from "../assets/music_listener.svg";
 import { fadeInAnimation } from "../services/animations";
 
 const AboutPage = () => {
   return (
     <Flex padding={5} animation={fadeInAnimation}>
-      <Heading>
-        a picture speaks a thousand words <br />
-        so should your playlists
-      </Heading>
+      <Center>
+        <Box paddingLeft={10}>
+          <Heading variant="normal" marginBottom={5}>
+            a picture speaks a thousand words
+          </Heading>
+          <Heading>so should your playlists</Heading>
+        </Box>
+      </Center>
       <Spacer />
       <Image src={musicListener} />
     </Flex>
